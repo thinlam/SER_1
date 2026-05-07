4871c86c0eecc59cc6fa4099d6aea20d48478b1e
hangtt <hangtt@vietinfo.tech>
2026-05-07 11:58:54 +0700

    iss 9451 - tình hình giải ngân nguồn vốn trong năm

---
+++ QLDA.Domain/DTOs/TinhHinhGiaiNganDto.cs
@@ -0,0 +1,14 @@
+
+
+namespace QLDA.Domain.DTOs;
+
+public record TinhHinhGiaiNganDto
+{
+    public int? LoaiDuAnId { get; set; }
+    public int? LoaiDuAnTheoNamId { get; set; }
+    public int? NguonVonId { get; set; }
+    public long? GiaTriHopDong { get; set; }
+    public long? GiaTriGiaiNgan   { get; set; }
+    public int Thang { get; set; }  
+    public int Nam { get; set; }
+}
